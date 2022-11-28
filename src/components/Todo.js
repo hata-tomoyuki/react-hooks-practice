1875de76d60da55dd233c3ad6679abbe37344a22
hata-tomoyuki <h.tomo0602@gmail.com>
2022-11-28 09:48:20 +0900

    create components

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './Form'
+import List from './List'
+
+const Todo = () => {
+  return (
+    <>
+        <List />
+        <Form />
+    </>
+  )
+}
+
+export default Todo
